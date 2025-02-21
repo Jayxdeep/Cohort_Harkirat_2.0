acea712c1cb54b064c2ff827853ad2a85cb1219e
Jaydeep Mukherjee <jaydeepmukherjee2003@gmail.com>
2025-02-21 15:28:22 +0530

    done changes

---
+++ Integers/hehe.js
@@ -0,0 +1,9 @@
+function explainParseInt(value){
+    console.log("origina Value:",value)
+    let res=parseInt(value);
+    console.log("after parseint:",res)
+}
+//usage of parseInt
+explainParseInt("42")
+explainParseInt("42px")
+explainParseInt("3.14")
